3a991b9d102bc73006a2fc33a175d47ae6c7cfe9
shmyh <shmyhol@gmail.com>
2019-09-11 23:31:01 -0400

    Finished 15 problems tried 16

--- CoreJavaHomework/src/nineteen/ArrayListAction.java
+++ CoreJavaHomework/src/nineteen/ArrayListAction.java
@@ -9,42 +9,42 @@ public class ArrayListAction {
 	}
 
 	private static void manipulate() {
-		int oddTotal=0, evenTotal=0;
-		ArrayList<Integer> AL=new ArrayList<Integer> ();
-		ArrayList<Integer> NonPrime=new ArrayList<Integer> ();
+		int oddTotal = 0, evenTotal = 0;
+		ArrayList<Integer> AL = new ArrayList<Integer>();
+		ArrayList<Integer> NonPrime = new ArrayList<Integer>();
 
-		for(int i=1;i<=10;i++) {
+		for (int i = 1; i < 11; i++) {
 			AL.add(i);
 		}
-		for(int i=0;i<AL.size();i++) {
-			int num=(int) AL.get(i);
-			if(num%2==0) {
-				evenTotal+=num;
-			}
-			else
-				oddTotal+=num;
-			
+		for (int i = 0; i < AL.size(); i++) {
+			int num = (int) AL.get(i);
+			if (num % 2 == 0) {
+				evenTotal += num;
+			} else
+				oddTotal += num;
+
 		}
 		System.out.println(evenTotal);
 		System.out.println(oddTotal);
-		
-		for (int i = 0; i < AL.size(); i++) {
-			for (int j = 2; j < i; ++j) {
-				System.out.println((AL.indexOf(i))+"%"+(AL.indexOf(j)));
-				if(((AL.indexOf(i))%(AL.indexOf(j)))==0) {
-					NonPrime.add(AL.indexOf(i));
+		int limit = AL.size();
+		// System.out.println(limit);
+			
+		for (int i = 0; i < limit; i++) {
+			// System.out.println(i);
+			 for (int j = 1; j < i; j++) {
+				System.out.println((AL.get(i)) + "%" + (AL.get(j)));
+				if (((AL.get(i)) % (AL.get(j))) == 0) {
+					// System.out.println(AL.indexOf(i));
+					NonPrime.add(AL.get(i));
 					break;
 				}
-				
+
 			}
 		}
-		for(int i=0;i<NonPrime.size();i++) {
-			System.out.println(NonPrime.get(i));
+		for (int i = 0; i < NonPrime.size(); i++) {
+			 System.out.println(NonPrime.get(i));
 		}
-		
-		
-		
-		
+
 	}
 
 }
