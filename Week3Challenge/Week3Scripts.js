29a5db23be54f738efa59e087421fa30dfb68afb
shmyh <shmyhol@gmail.com>
2019-09-23 13:48:13 -0400

    set an album list but no button

---
+++ Week3Challenge/Week3Scripts.js
@@ -0,0 +1,58 @@
+/*window.onload=function(){
+    document.getElementById("user").onclick=function(){
+        const apiUrl= "https://randomuser.me/documentation";
+        const ul=document.getElementById('users')
+        sendAjaxGet(url, getUser);
+    }
+}
+function createNode(element) {
+    return document.createElement(element);
+}
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+function sendAjaxGet(url,func){
+    let xhr= new XMLHttpRequest();
+    xhr.onreadystatechange=function(){
+        if(this.readyState===4 &&this.status===200){
+            func(this);
+        }
+    }
+
+
+}
+xhr.open("GET",url,true);
+xhr.send();
+*/
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ul=document.getElementById('users');
+
+const apiUrl= 'https://randomuser.me/api/?results=25';
+fetch(apiUrl)
+    .then((resp) => resp.json())
+    .then(function(data){
+        let users=data.results;
+        return users.map(function(user){
+            let li=createNode('li'),
+                img=createNode('img'),
+                span=createNode('span');
+            img.src=user.picture.medium;
+            span.innerHTML= "Name: "+`${user.name.first} ${user.name.last}`+"Age"+ `${user.dob.age}`;
+            append(li,img);
+            append(li, span);
+            append(ul, li);
+        })
+    
+    })
+    .catch((error)=>{
+        alert('oh no');
+        console.log(error);
+    });
+
